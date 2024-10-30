5a76f9cc5967c288a434b48308fe3e4b420bf5fe
PS <pramilanarawade70@gmail.com>
2024-10-30 12:18:57 +0530

    Update SharedPipeline.groovy

--- vars/SharedPipeline.groovy
+++ vars/SharedPipeline.groovy
@@ -6,10 +6,8 @@ def call() {
             DOCKERHUB_CREDENTIALS = credentials('dockerhubpwd')
             SLACK_CREDENTIALS = credentials('b3ee302b-e782-4d8e-ba83-7fa591d43205')
             SONARQUBE_CREDENTIALS = credentials('pipeline_Stoken') // SonarQube token credentials
-            SONARQUBE_SERVER = 'http://localhost:9000'   // Replace with your SonarQube server URL
-            ANCHORE_URL = 'http://192.168.1.6:8228' // Replace with your Anchore Engine URL
-            ANCHORE_USERNAME = 'admin' // Anchore username
-            ANCHORE_PASSWORD = 'foobar' // Anchore password
+            SONARQUBE_SERVER = 'http://localhost:9000'
+            ANCHORE_IMAGE_NAME = "${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}"
         }
 
         parameters {
@@ -43,7 +41,7 @@ def call() {
             stage('SonarQube Analysis') {
                 steps {
                     dir('testhello') {
-                        withSonarQubeEnv('SonarQube') { // 'SonarQube' is the name defined in Jenkins global configuration
+                        withSonarQubeEnv('SonarQube') {
                             sh '''
                                 mvn sonar:sonar \
                                     -Dsonar.projectKey=testhello \
@@ -88,19 +86,7 @@ def call() {
 
             stage('Scan Image with Anchore') {
                 steps {
-                    script {
-                        // Create a new image in Anchore
-                        sh """
-                            curl -X POST -u '${ANCHORE_USERNAME}:${ANCHORE_PASSWORD}' -H 'Content-Type: application/json' -d '{"tag": "${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}"}' ${ANCHORE_URL}/v1/images
-                        """
-                        
-                        // Wait for the image to be processed
-                        sleep(time: 30, unit: 'SECONDS')
-
-                        // Get the image scan results
-                        def results = sh(script: "curl -s -u '${ANCHORE_USERNAME}:${ANCHORE_PASSWORD}' ${ANCHORE_URL}/v1/images/${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}/report", returnStdout: true)
-                        echo "Scan Results: ${results}"
-                    }
+                    anchore name: ANCHORE_IMAGE_NAME, engineRetries: 3, policyEval: true
                 }
             }
 
@@ -119,51 +105,19 @@ def call() {
         post {
             always {
                 script {
-                    def slackBaseUrl = 'https://slack.com/api/'
-                    def slackChannel = '#builds'
                     def slackColor = currentBuild.currentResult == 'SUCCESS' ? 'good' : 'danger'
-                    def slackMessage = "Build ${currentBuild.fullDisplayName} finished with status: ${currentBuild.currentResult}"
-
-                    echo "Sending Slack notification to ${slackChannel} with message: ${slackMessage}"
-
-                    // Send Slack notification
                     slackSend(
-                        baseUrl: 'https://yourteam.slack.com/api/',
-                        teamDomain: 'StarAppleInfotech',
                         channel: '#builds',
                         color: slackColor,
-                        botUser: true,
-                        tokenCredentialId: SLACK_CREDENTIALS,
-                        notifyCommitters: false,
-                        message: "Build Java Application #${env.BUILD_NUMBER} finished with status: ${currentBuild.currentResult}"
+                        message: "Build ${currentBuild.fullDisplayName} finished with status: ${currentBuild.currentResult}",
+                        tokenCredentialId: SLACK_CREDENTIALS
                     )
                 }
-
-                // Send email notification
+                
                 emailext(
                     to: 'pramila.narawadesv@gmail.com',
                     subject: "Jenkins Build ${env.JOB_NAME} #${env.BUILD_NUMBER} ${currentBuild.currentResult}",
                     body: """<p>Build ${env.JOB_NAME} #${env.BUILD_NUMBER} finished with status: ${currentBuild.currentResult}</p>
-                            <p>Check console output at ${env.BUILD_URL}</p>""",
-                    mimeType: 'text/html'
-                )
-            }
-
-            failure {
-                emailext(
-                    to: 'pramila.narawadesv@gmail.com',
-                    subject: "Jenkins Build ${env.JOB_NAME} #${env.BUILD_NUMBER} Failed",
-                    body: """<p>Build ${env.JOB_NAME} #${env.BUILD_NUMBER} failed.</p>
-                            <p>Check console output at ${env.BUILD_URL}</p>""",
-                    mimeType: 'text/html'
-                )
-            }
-
-            success {
-                emailext(
-                    to: 'pramila.narawadesv@gmail.com',
-                    subject: "Jenkins Build ${env.JOB_NAME} #${env.BUILD_NUMBER} Succeeded",
-                    body: """<p>Build ${env.JOB_NAME} #${env.BUILD_NUMBER} succeeded.</p>
                             <p>Check console output at ${env.BUILD_URL}</p>""",
                     mimeType: 'text/html'
                 )
