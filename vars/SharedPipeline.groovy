b8ad4db0cc260d32072fb9be44d4f1e809bca555
PS <pramilanarawade70@gmail.com>
2024-10-30 19:29:23 +0530

    Update SharedPipeline.groovy

--- vars/SharedPipeline.groovy
+++ vars/SharedPipeline.groovy
@@ -5,10 +5,11 @@ def call() {
         environment {
             DOCKERHUB_CREDENTIALS = credentials('dockerhubpwd')
             SLACK_CREDENTIALS = credentials('b3ee302b-e782-4d8e-ba83-7fa591d43205')
-            SONARQUBE_CREDENTIALS = credentials('pipeline_Stoken')
-            SONARQUBE_SERVER = 'http://localhost:9000'
-            ANCHORE_ENGINE_CREDENTIALS = credentials('anchor_id')
-            ANCHORE_URL = 'http://192.168.1.6:8228'
+            SONARQUBE_CREDENTIALS = credentials('pipeline_Stoken') // SonarQube token credentials
+            SONARQUBE_SERVER = 'http://localhost:9000'   // Replace with your SonarQube server URL
+            ANCHORE_URL = 'http://192.168.1.6:8228' // Replace with your Anchore Engine URL
+            ANCHORE_USERNAME = 'admin' // Anchore username
+            ANCHORE_PASSWORD = 'foobar' // Anchore password
         }
 
         parameters {
@@ -31,7 +32,7 @@ def call() {
                         sh 'mvn clean install'
                         script {
                             def image = docker.build("${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}")
-                            docker.withRegistry('', DOCKERHUB_CREDENTIALS) {
+                            docker.withRegistry('', 'dockerhubpwd') {
                                 image.push()
                             }
                         }
@@ -42,13 +43,13 @@ def call() {
             stage('SonarQube Analysis') {
                 steps {
                     dir('testhello') {
-                        withSonarQubeEnv('SonarQube') {
-                            sh """
+                        withSonarQubeEnv('SonarQube') { // 'SonarQube' is the name defined in Jenkins global configuration
+                            sh '''
                                 mvn sonar:sonar \
                                     -Dsonar.projectKey=testhello \
                                     -Dsonar.host.url=${SONARQUBE_SERVER} \
-                                    -Dsonar.login=${SONARQUBE_CREDENTIALS_PSW}
-                            """
+                                    -Dsonar.login=${SONARQUBE_CREDENTIALS}
+                            '''
                         }
                     }
                 }
@@ -85,28 +86,20 @@ def call() {
                 }
             }
 
-            stage('Anchore Scan') {
+            stage('Scan Image with Anchore') {
                 steps {
                     script {
-                        withCredentials([usernamePassword(credentialsId: 'anchor_id', passwordVariable: 'ANCHORE_PASSWORD', usernameVariable: 'ANCHORE_USERNAME')]) {
-                            sh """
-                                anchore-cli --u $ANCHORE_USERNAME --p $ANCHORE_PASSWORD --url $ANCHORE_URL image add ${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}
-                                anchore-cli --u $ANCHORE_USERNAME --p $ANCHORE_PASSWORD --url $ANCHORE_URL image wait ${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}
-                            """
+                        // Create a new image in Anchore
+                        sh """
+                            curl -X POST -u '${ANCHORE_USERNAME}:${ANCHORE_PASSWORD}' -H 'Content-Type: application/json' -d '{"tag": "${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}"}' ${ANCHORE_URL}/v1/images
+                        """
+                        
+                        // Wait for the image to be processed
+                        sleep(time: 30, unit: 'SECONDS')
 
-                            def scanResult = sh(
-                                script: """
-                                    anchore-cli --u $ANCHORE_USERNAME --p $ANCHORE_PASSWORD --url $ANCHORE_URL image vuln ${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number} all || true
-                                """,
-                                returnStdout: true
-                            ).trim()
-
-                            echo "Anchore Scan Results for ${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}:\n${scanResult}"
-
-                            if (scanResult.contains("High")) {
-                                error "Anchore scan found high severity vulnerabilities. Failing the build."
-                            }
-                        }
+                        // Get the image scan results
+                        def results = sh(script: "curl -s -u '${ANCHORE_USERNAME}:${ANCHORE_PASSWORD}' ${ANCHORE_URL}/v1/images/${params.DOCKERHUB_USERNAME}/${params.JAVA_IMAGE_NAME}:${currentBuild.number}/report", returnStdout: true)
+                        echo "Scan Results: ${results}"
                     }
                 }
             }
@@ -126,20 +119,27 @@ def call() {
         post {
             always {
                 script {
+                    def slackBaseUrl = 'https://slack.com/api/'
+                    def slackChannel = '#builds'
                     def slackColor = currentBuild.currentResult == 'SUCCESS' ? 'good' : 'danger'
                     def slackMessage = "Build ${currentBuild.fullDisplayName} finished with status: ${currentBuild.currentResult}"
 
-                    echo "Sending Slack notification with message: ${slackMessage}"
+                    echo "Sending Slack notification to ${slackChannel} with message: ${slackMessage}"
 
+                    // Send Slack notification
                     slackSend(
-                        baseUrl: 'https://slack.com/api/',
+                        baseUrl: 'https://yourteam.slack.com/api/',
+                        teamDomain: 'StarAppleInfotech',
                         channel: '#builds',
                         color: slackColor,
+                        botUser: true,
                         tokenCredentialId: SLACK_CREDENTIALS,
+                        notifyCommitters: false,
                         message: "Build Java Application #${env.BUILD_NUMBER} finished with status: ${currentBuild.currentResult}"
                     )
                 }
 
+                // Send email notification
                 emailext(
                     to: 'pramila.narawadesv@gmail.com',
                     subject: "Jenkins Build ${env.JOB_NAME} #${env.BUILD_NUMBER} ${currentBuild.currentResult}",
